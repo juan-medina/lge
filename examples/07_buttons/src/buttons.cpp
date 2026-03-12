677cb4f3769b400a778597247f663cf50dbdcada
Juan Medina <jamedina@gmail.com>
2026-03-12 22:58:26 +0000

    Add Kenney UI sprite pack & update buttons example
    Add the Kenney 'UI Pack - Pixel Adventure' assets under examples/media/sprites/kenney_ui-pack-pixel-adventure (License.txt, Preview.png, Sample.png and a large set of tile PNGs). Also modify examples/07_buttons/src/buttons.cpp and buttons.hpp to integrate/use the new UI sprites in the buttons example. Includes licensing file for the added assets.

--- examples/07_buttons/src/buttons.cpp
+++ examples/07_buttons/src/buttons.cpp
@@ -4,6 +4,43 @@
 #include "buttons.hpp"
 
 #include <lge/app/main.hpp>
+#include <lge/components/placement.hpp>
+#include <lge/components/sprite.hpp>
+#include <lge/core/result.hpp>
+#include <lge/interface/resource_manager.hpp>
+
+#include "../../src/example.hpp"
+
+#include <entt/core/hashed_string.hpp>
 
 LGE_MAIN(examples::buttons);
 
+namespace examples {
+
+using entt::literals::operator""_hs;
+
+auto buttons::init() -> lge::result<> {
+	if(const auto err = example::init().unwrap(); err) [[unlikely]] {
+		return lge::error("error init the app", *err);
+	}
+
+	if(const auto err = ctx.resources.load_sprite_sheet(ui_sheet_path).unwrap(ui_sheet_); err) [[unlikely]] {
+		return lge::error("failed to load ui sprite sheet", *err);
+	}
+
+	ui_sprite_ = ctx.world.create();
+	ctx.world.emplace<lge::sprite>(ui_sprite_, ui_sheet_, "tile_0017.png"_hs);
+	ctx.world.emplace<lge::placement>(ui_sprite_, 0.0F, 0.0F);
+
+	return true;
+}
+
+auto buttons::end() -> lge::result<> {
+	if(const auto err = ctx.resources.unload_sprite_sheet(ui_sheet_).unwrap(); err) [[unlikely]] {
+		return lge::error("failed to unload ui sprite sheet", *err);
+	}
+
+	return example::end();
+}
+
+} // namespace examples
--- examples/07_buttons/src/buttons.hpp
+++ examples/07_buttons/src/buttons.hpp
@@ -3,7 +3,13 @@
 
 #pragma once
 
+#include <lge/core/result.hpp>
+#include <lge/interface/resources.hpp>
+
 #include "../../src/example.hpp"
+
+#include <entt/entity/entity.hpp>
+#include <entt/entity/fwd.hpp>
 
 namespace examples {
 
@@ -11,13 +17,21 @@ class buttons: public example {
 public:
 	explicit buttons(): example(example_title, kb_message, controller_message) {}
 
+protected:
+	[[nodiscard]] auto init() -> lge::result<> override;
+	[[nodiscard]] auto end() -> lge::result<> override;
+
 private:
 	static constexpr auto example_title = "Buttons!";
 	static constexpr auto kb_message =
 		"Press {#FFFF00}F5{#}: debug draw, {#FFFF00}F11{#}: toggle fullscreen, {#FFFF00}Esc{#}: exit";
 	static constexpr auto controller_message =
 		"Press {#FFFF00}START{#}: debug draw, {#FFFF00}SELECT{#}: toggle fullscreen, {#FFFF00}B{#}: exit";
+
+	static constexpr auto ui_sheet_path = "resources/game/sprites/ui.json";
+
+	lge::sprite_sheet_handle ui_sheet_{lge::invalid_sprite_sheet};
+	entt::entity ui_sprite_{entt::null};
 };
 
 } // namespace examples
-
